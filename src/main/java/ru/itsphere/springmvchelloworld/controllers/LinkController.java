3eaad8b7e76642ad1747103279aa82dce1571eea
Your Name <you@example.com>
2016-03-01 16:01:26 +0300

    first commit

---
+++ src/main/java/ru/itsphere/springmvchelloworld/controllers/LinkController.java
@@ -0,0 +1,31 @@
+package ru.itsphere.springmvchelloworld.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LinkController {
+
+    public static final String METHOD_NAME_ATTR = "methodName";
+    public static final String CONTROLLER_PAGE = "controllerPage";
+
+    @RequestMapping(method = RequestMethod.GET, value = "/first")
+    public String firstMethod(ModelMap model) {
+        model.addAttribute(METHOD_NAME_ATTR, "First");
+        return CONTROLLER_PAGE;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/second")
+    public String secondMethod(ModelMap model) {
+        model.addAttribute(METHOD_NAME_ATTR, "Second");
+        return CONTROLLER_PAGE;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/third")
+    public String thirdMethod(ModelMap model) {
+        model.addAttribute(METHOD_NAME_ATTR, "Third");
+        return CONTROLLER_PAGE;
+    }
+}
---
+++ src/main/java/ru/itsphere/springmvchelloworld/controllers/StartupController.java
@@ -0,0 +1,15 @@
+package ru.itsphere.springmvchelloworld.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class StartupController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String dispatchToHomePage() {
+        return "home";
+    }
+}
